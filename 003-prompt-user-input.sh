3a9aea1655f90fb3847385e76bb5c36880458ab0
Aziz <akashiseijurou414@gmail.com>
2024-11-02 09:15:10 +0700

    add user interactive prompt input

---
+++ 003-prompt-user-input.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+echo "Enter your name: "
+read name
+
+echo "Hey $name, whats up buddy"
+
+echo "----> another way"
+read -p "who are yo? " user
+
+echo "hello $user, whats up dude"
