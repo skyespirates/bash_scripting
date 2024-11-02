3d71d7cb9da390102ab00a505c8dc6edd10805e7
Aziz <akashiseijurou414@gmail.com>
2024-11-02 09:10:06 +0700

    add accept & store cli arguments

---
+++ 002-cli-arguments.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+# this program will accepts arguments from cli and display it
+# ./002-cli-arguments.sh skyes crawford tintin
+echo "first params: $1"
+echo "second params: $2"
+echo "all params: $@"
